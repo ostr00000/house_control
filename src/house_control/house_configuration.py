9ce06cd3624125b8fd7e2f811c6776d9503ac69f
ostro <ostr00000@gmail.com>
2019-12-30 22:38:46 +0100

    fix house config

--- src/house_control/house_configuration.py
+++ src/house_control/house_configuration.py
@@ -2,6 +2,7 @@ from house_control.model.device.impl import Computer, Monitor, Light, ColorLight
     Fan, SpeedFan, Blower, Radio, TV, AlarClock, Shutter
 from house_control.model.location import Loc
 
+lamps = []
 house = Loc('dom')
 
 attic = Loc('poddasze', parent=house)
@@ -12,54 +13,77 @@ bathroom = Loc('łazienka', parent=house)
 livingRoom = Loc('salon', parent=house)
 kitchen = Loc('kuchnia', parent=house)
 
-# attic part 1
-Light('oświetlenie górne', atticP1)
-Light('oświetlenie biurka', atticP1)
-Radio('radio', atticP1)
-Computer('komputer', atticP1)
-Monitor('monitor', atticP1)
+# ################ attic part 1 ################
+curLoc = atticP1
+lamps.extend([
+    Light('oświetlenie górne', curLoc),
+    Light('oświetlenie biurka', curLoc)
+])
+Radio('radio', curLoc)
+Computer('komputer', curLoc)
+Monitor('monitor', curLoc)
 
-# attic part 2
-Light('oświetlenie górne', atticP2)
-Light('lampa nad ławą', atticP2)
-Light('lampa lewa', atticP2)
-Light('lampa prawa', atticP2)
-Light('lampa', atticP2)
-TV('telewizor', atticP2)
-Light('oświetlenie za telewizorem', atticP2)
-Light('całe oświetlenie na poddaszu', atticP2)
+# ################ attic part 2 ################
+curLoc = atticP2
+lamps.extend([
+    Light('oświetlenie górne', curLoc),
+    Light('lampa nad ławą', curLoc),
+    Light('lampa lewa', curLoc),
+    Light('lampa prawa', curLoc),
+    Light('lampa', curLoc),
+])
+TV('telewizor', curLoc)
+lamp5 = Light('oświetlenie za telewizorem', curLoc)
+Light('całe oświetlenie na poddaszu', curLoc, aggr=lamps)
+lamps.clear()
 
-# bedroom
-ColorLight('oświetlenie górne', bedroom)
-Light('lampa lewa', bedroom)
-Light('lampa prawa', bedroom)
-Light('lampa', bedroom)
-TV('telewizor', bedroom)
-Light('oświetlenie za telewizorem', bedroom)
-Light('całe oświetlenie w sypialni', bedroom)
-AlarClock('budzik', atticP2)
+# ################ bedroom ################
+curLoc = bedroom
+lamps.extend([
+    ColorLight('oświetlenie górne', curLoc),
+    Light('lampa lewa', curLoc),
+    Light('lampa prawa', curLoc),
+    Light('lampa', curLoc),
+    Light('oświetlenie za telewizorem', curLoc),
+])
 
-# bathroom
-Light('oświetlenie górne', bathroom)
-Light('lampa nad lustrem', bathroom)
-Fan('wentylator', bathroom)
-Blower('dmuchawa', bathroom)
-Light('całe oświetlenie w łazience', bathroom)
+TV('telewizor', curLoc)
+Light('całe oświetlenie w sypialni', curLoc, aggr=lamps)
+AlarClock('budzik', curLoc)
+lamps.clear()
 
-# living room
-Light('oświetlenie górne', livingRoom)
-ColorLight('lampa lewa', livingRoom)
-ColorLight('lampa prawa', livingRoom)
-Light('całe oświetlenie w salonie', livingRoom)
-Shutter('Roleta lewa', livingRoom)
-Shutter('Roleta prawa', livingRoom)  # TODO default should be 'razem'
-Shutter('Rolety razem', livingRoom)
+# ################ bathroom ################
+curLoc = bathroom
+lamps.extend([
+    Light('oświetlenie górne', curLoc),
+    Light('lampa nad lustrem', curLoc),
+])
+Fan('wentylator', curLoc)
+Blower('dmuchawa', curLoc)
+Light('całe oświetlenie w łazience', curLoc, aggr=lamps)
+lamps.clear()
 
-# kitchen
-Light('oświetlenie górne', kitchen)
-BrightnessLight('oświetlenie nad szafkami', kitchen)
-Light('oświetlenie pod szafkami', kitchen)
-Light('oświetlenie okapu', kitchen)
-SpeedFan('okap', kitchen)
-Fan('wentylator', kitchen)
-Light('całe oświetlenie w kuchni', livingRoom)
+# ################ livingRoom ################
+curLoc = livingRoom
+lamps.extend([
+    Light('oświetlenie górne', curLoc),
+    ColorLight('lampa lewa', curLoc),
+    ColorLight('lampa prawa', curLoc),
+])
+Light('całe oświetlenie w salonie', curLoc, aggr=lamps)
+lamps.clear()
+s0 = Shutter('Roleta lewa', curLoc)
+s1 = Shutter('Roleta prawa', curLoc)
+Shutter('Rolety razem', curLoc, aggr=[s0, s1])
+
+# ################ kitchen ################
+curLoc = kitchen
+lamps.extend([
+    Light('oświetlenie górne', curLoc),
+    BrightnessLight('oświetlenie nad szafkami', curLoc),
+    Light('oświetlenie pod szafkami', curLoc),
+    Light('oświetlenie okapu', curLoc),
+])
+SpeedFan('okap', curLoc)
+Fan('wentylator', curLoc)
+Light('całe oświetlenie w kuchni', curLoc, aggr=lamps)
