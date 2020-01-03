d88edfaed1b57062e2bce16e11d2f712c7bf4179
ostro <ostr00000@gmail.com>
2020-01-03 01:39:39 +0100

    add color formatting

--- test/doc_example.py
+++ test/doc_example.py
@@ -1,6 +1,7 @@
 import logging
+from functools import partial, partialmethod
 from random import seed
-from typing import Iterator, Tuple
+from typing import Iterator, Tuple, Callable
 
 import xlrd
 
@@ -20,12 +21,32 @@ def xlsTestGenerator() -> Iterator[Tuple[str, str]]:
         yield command.value, symbol.value
 
 
+class Color:
+    _NORMAL = '\033[0m'
+    _RED = '\033[31m'
+    _GREEN = '\033[32m'
+    _MAGENTA = '\033[35m'
+
+    @staticmethod
+    def _logInColor(text, color=None, level=logging.INFO):
+        logger.log(level, f'{color} {text} {Color._NORMAL}')
+
+    red = partialmethod(_logInColor, color=_RED)
+    magenta = partialmethod(_logInColor, color=_MAGENTA)
+    green = partialmethod(_logInColor, color=_GREEN)
+
+
 def testFromXls():
-    """Non empty results 115/214"""
+    """
+    Non empty results: 110/214
+    Fully recognized command: 32/214
+    """
     rec = Recognizer(house)
 
     total = 0
     nonEmpty = 0
+    fullyRecognizedCommand = 0
+
     gen = xlsTestGenerator()
     header = next(gen)
     logger.info(f'{" " * 12}{header[0].ljust(30)} : {header[1]}')
@@ -36,11 +57,23 @@ def testFromXls():
         if event is not None:
             nonEmpty += 1
 
-        logger.info(
-            f"Recognized: {str(event).ljust(30)} : "
-            f"Expected: {symbol.ljust(15)} : "
-            f"{repr(event).ljust(30)} : "
+        recognized = symbol == str(event)
+        if recognized:
+            fullyRecognizedCommand += 1
+
+        if recognized:
+            color: Callable = Color.green
+        elif event is not None:
+            color = Color.magenta
+        else:
+            color = Color.red
+
+        color(
+            f"Recognized: {str(event).ljust(22)} "
+            f"Expected: {symbol.ljust(18)} "
+            f"Command: {command.ljust(40)} "
+            f"{repr(event).ljust(30)} "
         )
-        rec.recognizeOptionalEvent(command)
 
     logger.info(f"Non empty results {nonEmpty}/{total}")
+    logger.info(f"Fully recognized command {fullyRecognizedCommand}/{total}")
