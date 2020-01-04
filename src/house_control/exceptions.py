bd012a756bd7772504483cce972f1cb9404d9dd0
ostro <ostr00000@gmail.com>
2020-01-04 17:06:50 +0100

    improve exceptions

--- src/house_control/exceptions.py
+++ src/house_control/exceptions.py
@@ -1,17 +1,29 @@
 class RecogniseException(Exception):
-    pass
+    def __init__(self, *args, **kwargs):
+        self.args = args
+        self.kwargs = dict(kwargs)
+
+    def __str__(self):
+        res = ''
+        if args := ','.join(str(a) for a in self.args):
+            res += args
+        if kwargs := ','.join(f'{k}={v}' for k, v in self.kwargs.items()):
+            if res:
+                res += ', '
+            res += kwargs
+        return res
 
 
 class UnknownAction(RecogniseException):
     def __str__(self):
-        return "Cannot recognize action"
+        return f"Cannot recognize action {super().__str__()}"
 
 
 class UnknownLocation(RecogniseException):
     def __str__(self):
-        return "Cannot recognize location"
+        return f"Cannot recognize location {super().__str__()}"
 
 
 class UnknownDevice(RecogniseException):
     def __str__(self):
-        return "Cannot recognize device"
+        return f"Cannot recognize device {super().__str__()}"
